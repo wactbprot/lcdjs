32cf661b48f431a67f0a34d99e74dae34a13567d
wactbprot <thsteinbock@web.de>
2013-10-10 19:15:24 +0000

    ..

--- lcd.js
+++ lcd.js
@@ -10,45 +10,49 @@ var gpio = require('gpio'),
 // here we use raspberry pi numering
 // not BCM
 lcd_setup = function(cbfn){
-    lcd_rs = gpio.export(11, 
-		                     {direction: 'out',
-		                      ready: function() {
-		                          console.log("go for 11");
-			                        lcd_e  = gpio.export(10, {direction: 'out',
-						                                            ready: function() {
-							                                              console.log("go for 10");
-							                                              lcd_d4 = gpio.export(4,  {direction: 'out',
-										                                                                  ready: function() {
-										                                                                      console.log("go for 6");
-										                                                                      lcd_d5 = gpio.export(5,  {direction: 'out',
-														                                                                                        ready: function() {
-														                                                                                            console.log("go for 5");
-														                                                                                            lcd_d6 = gpio.export(6,  {direction: 'out',
-																	                                                                                                                ready: function() {
-																		                                                                                                                  console.log("go for 4");
-														
-																		                                                                                                                  lcd_d7 = gpio.export(1,  {direction: 'out',
-																					                                                                                                                                      ready: function() {
-																						                                                                                                                                        console.log("go for 1");
-   																						                                                                                                                                      if(typeof cbfn == 'function'){
-   																						                                                                                                                                          cbfn();
-   																						                                                                                                                                      }else{
-   																						                                                                                                                                          console.log("ok");
-   																						                                                                                                                                          return true;
-   																						                                                                                                                                      };
-																						
-																					                                                                                                                                      }});
-																		                                                                                                                  
-																	                                                                                                                }});
-														                                                                                            
-														                                                                                        }});
-										                                                                      
-										                                                                  }});
-							                                              
-						                                            }});
-			                        
-		                      }
-		     });
+    lcd_rs = gpio
+        .export(11,
+                {direction: 'out',
+                 ready: function() {
+                     console.log("go for 11");
+                     lcd_e  = gpio
+                         .export(10, {direction: 'out',
+                                      ready: function() {
+                                          console.log("go for 10");
+                                          lcd_d4 = gpio
+                                              .export(4,  {direction: 'out',
+                                                           ready: function() {
+                                                               console.log("go for 6");
+                                                               lcd_d5 = gpio
+                                                                   .export(5,  {direction: 'out',
+                                                                                ready: function() {
+                                                                                    console.log("go for 5");
+                                                                                    lcd_d6 = gpio
+                                                                                        .export(6,  {direction: 'out',
+                                                                                                     ready: function() {
+                                                                                                         console.log("go for 4");
+                                                                                                         lcd_d7 = gpio
+                                                                                                             .export(1,  {direction: 'out',
+                                                                                                                          ready: function() {
+                                                                                                                              console.log("go for 1");
+                                                                                                                              if(typeof cbfn == 'function'){
+                                                                                                                                  cbfn();
+                                                                                                                              }else{
+                                                                                                                                  console.log("ok");
+                                                                                                                                  return true;
+                                                                                                                              };
+                                                                                                                          }});
+
+                                                                                                     }});
+
+                                                                                }});
+
+                                                           }});
+
+                                      }});
+
+                 }
+                });
 };
 
 // Define some device constants
@@ -56,141 +60,153 @@ var LCD_WIDTH = 20,   // Maximum characters per line
 LCD_CHR = true,
 LCD_CMD = false,
 // Timing constants
-E_PULSE = 1, // ms
-E_DELAY = 1; // ms
+E_PULSE = 50, // ms
+E_DELAY = 50; // ms
 
 var lcd_line = new Buffer(4);
 
 lcd_line[0] = 0x80; // LCD RAM address for the 1st line
-  lcd_line[1] = 0xC0; // LCD RAM address for the 2nd line
+lcd_line[1] = 0xC0; // LCD RAM address for the 2nd line
 lcd_line[2] = 0x94; // LCD RAM address for the 3rd line
 lcd_line[3] = 0xD4; // LCD RAM address for the 4th line
 
 var filter = new Buffer(4);
 filter[0] = 0x10;
-  filter[1] = 0x20;
-  filter[2] = 0x30;
-  filter[3] = 0x40;
-  
-  var init = new Buffer(6);
-  init[0] = 0x33;
-  init[1] = 0x32;
-  init[2] = 0x28;
-  init[3] = 0x0C;
-  init[4] = 0x06;
-  init[5] = 0x01;
-  
-  var lcd_init = function(cbfn){
-     lcd_byte(init[0],LCD_CMD,
-   	   function(){
-   	       lcd_byte(init[1],LCD_CMD,
-   			function(){
-   			    lcd_byte(init[2],LCD_CMD,
-   				     function(){
-   					 lcd_byte(init[3],LCD_CMD,  
-   						  function(){
-   						      lcd_byte(init[4],LCD_CMD,
-   							       function(){
-   								   lcd_byte(init[5],LCD_CMD, cbfn)}
-   							      )}
-   						 )}
-   				    )}
-   		       )}
-   	  );  
-   
-   
-   };
-//   
-   var lcd_byte = function(bits, mode, cbfn){
-       //   Send byte to data pins
-       //   bits = data
-       //   mode = True  for character
-       //          False for command
-   
-       var fn1 = function(){
-           //console.log("1");
-   	// RS
-   	if(mode){
-               lcd_rs.set();
-   	}else{
-               lcd_rs.reset();
-   	}   
-       }
-   
-       var fn25 = function(){
-           //console.log("25");
-           // High bits
-           lcd_d4.reset();
-           lcd_d5.reset();
-           lcd_d6.reset();
-           lcd_d7.reset();
-   
-           if( bits&filter[0]==filter[0]){
-               lcd_d4.set();
-           }
-   
-           if( bits&filter[1]==filter[1]){
-               lcd_d5.set();
-           }
-   
-           if( bits&filter[2]==filter[2]){
-               lcd_d6.set();
-           }
-   
-           if( bits&filter[3]==filter[3]){
-               lcd_d7.set();
-           }
-       }
-   
-       //Toggle 'Enable' pin
-   
-       //time.sleep(E_DELAY)
-       var fn36 = function(){
-          // console.log("36");
-           lcd_e.set();
-       }
-       //time.sleep(E_PULSE)
-       var fn47 = function(){
-          // console.log("47");
-          lcd_e.reset();
-       }
-       // time.sleep(E_DELAY)
-//       
-       setTimeout(function(){
-   	fn1();
-   
-   	setTimeout(function(){
-                 fn25();
-   
-              setTimeout(function(){
-                  fn36();
-   
-                  setTimeout(function(){
-                      fn47();
-   
-                      setTimeout(function(){
-                          fn25();
-   
-                          setTimeout(function(){
-                              fn36();
-   
-                              setTimeout(function(){
-                                  fn47();
-   
-   			       if(typeof cbfn == 'function'){
-   				   cbfn();
-   			       }else{
-   				   console.log("ok");
-   				   return true;
-   			       };
-   			   },E_DELAY);
-                          },E_DELAY);
-                      },E_DELAY);
-                  },E_DELAY);
-              },E_DELAY);
-          },E_DELAY);
-       }, E_DELAY);
-   }
-      
-   
-  lcd_setup();
+filter[1] = 0x20;
+filter[2] = 0x30;
+filter[3] = 0x40;
+
+var init = new Buffer(6);
+init[0] = 0x33;
+init[1] = 0x32;
+init[2] = 0x28;
+init[3] = 0x0C;
+init[4] = 0x06;
+init[5] = 0x01;
+
+var lcd_init = function(cbfn){
+    lcd_byte(init[0],LCD_CMD,
+             function(){
+                 lcd_byte(init[1],LCD_CMD,
+                          function(){
+                              lcd_byte(init[2],LCD_CMD,
+                                       function(){
+                                           lcd_byte(init[3],LCD_CMD,
+                                                    function(){
+                                                        lcd_byte(init[4],LCD_CMD,
+                                                                 function(){
+                                                                     lcd_byte(init[5],LCD_CMD, cbfn)}
+                                                                )}
+                                                   )}
+                                      )}
+                         )}
+            );
+
+
+};
+//
+var lcd_byte = function(bits, mode, cbfn){
+    //   Send byte to data pins
+    //   bits = data
+    //   mode = True  for character
+    //          False for command
+
+    var fn1 = function(){
+        console.log("1");
+        // RS
+        if(mode){
+            lcd_rs.set();
+        }else{
+            lcd_rs.reset();
+        }
+    }
+
+    var fn25 = function(){
+        console.log("25");
+        // High bits
+        lcd_d4.reset(function(){
+            console.log("reset d4");
+            lcd_d5.reset(function(){
+                console.log("reset d5");
+                lcd_d6.reset(function(){
+                    console.log("reset d6");
+                    lcd_d7.reset(function(){
+                        if( bits&filter[0]==filter[0]){
+                            lcd_d4.set(
+                                function(){console.log("set d4");
+                                          });
+                        }
+                        if( bits&filter[1]==filter[1]){
+                            lcd_d5.set(
+                                function(){console.log("set d5");
+                                          );
+                            }
+                            
+                            if( bits&filter[2]==filter[2]){
+                                lcd_d6.set(
+                                    function(){console.log("set d6");
+                                              );
+                                }
+                                
+                                if( bits&filter[3]==filter[3]){
+                                    lcd_d7.set(
+                                        function(){console.log("set d7");
+                                                  );
+                                    }
+                                })
+                        })
+                                })
+                            })
+                         
+                         //Toggle 'Enable' pin
+
+    //time.sleep(E_DELAY)
+    var fn36 = function(){
+        // console.log("36");
+        lcd_e.set();
+    }
+    //time.sleep(E_PULSE)
+    var fn47 = function(){
+        // console.log("47");
+        lcd_e.reset();
+    }
+    // time.sleep(E_DELAY)
+    //
+    setTimeout(function(){
+        fn1();
+
+        setTimeout(function(){
+            fn25();
+
+            setTimeout(function(){
+                fn36();
+
+                setTimeout(function(){
+                    fn47();
+
+                    setTimeout(function(){
+                        fn25();
+
+                        setTimeout(function(){
+                            fn36();
+
+                            setTimeout(function(){
+                                fn47();
+
+                                if(typeof cbfn == 'function'){
+                                    cbfn();
+                                }else{
+                                    console.log("ok");
+                                    return true;
+                                };
+                            },E_DELAY);
+                        },E_DELAY);
+                    },E_DELAY);
+                },E_DELAY);
+            },E_DELAY);
+        },E_DELAY);
+    }, E_DELAY);
+}
+
+
+lcd_setup();
